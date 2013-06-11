5be6f39fd5a6930679b3f1d7318e6518f57ddf86
Les Smithson <lsmithso@hare.demon.co.uk>
2013-06-11 06:54:41 +0100

    new project

---
+++ test/vr.py
@@ -0,0 +1,17 @@
+"""
+audioconvert ! '\
+'level message=true interval=5000000000
+"""
+
+import sys, urllib2, pprint, json
+
+url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-GB&maxresults=1"
+flac=open(sys.argv[1],"rb").read()                                                       
+header = {'Content-Type' : 'audio/x-flac; rate=16000'}                                   
+req = urllib2.Request(url, flac, header)                                                 
+data = json.loads(urllib2.urlopen(req).read()                                                              )
+print data
+h = data['hypotheses']
+print h[0]['utterance']
+print h[0]['confidence']
+
